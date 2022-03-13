faee6c1947e3b4fc1765b4df063d1c63b9ec1100
Aleks164 <alekseylehamail.ru>
2022-03-13 23:24:17 +0300

    fix type error

--- src/createAsincRender.ts
+++ src/createAsincRender.ts
@@ -11,22 +11,22 @@ type IsItonLeave = undefined | boolean;
 
 export const createAsincRender =
   (content: string, sec: number, isItonLeave: IsItonLeave = undefined) =>
-  (...args: Args[]) => {
-    windowHistory(args as unknown as Args);
-    messageCreator(content, sec, isItonLeave);
-    const rootEl = document.getElementById("root");
-    return new Promise((resolve) => {
-      if (rootEl) {
-        if (args[0].state) {
-          rootEl.innerHTML = `<h2>Loading state...</h2>`;
-        }
-        setTimeout(() => {
-          if (rootEl) {
-            rootEl.innerHTML = ``;
-            rootEl.innerHTML += `<h2>state: ${args[0].state}</h2>`;
-            resolve("ok");
+    (...args: Args[]) => {
+      windowHistory(args);
+      messageCreator(content, sec, isItonLeave);
+      const rootEl = document.getElementById("root");
+      return new Promise((resolve) => {
+        if (rootEl) {
+          if (args[0].state) {
+            rootEl.innerHTML = `<h2>Loading state...</h2>`;
           }
-        }, sec);
-      }
-    });
-  };
+          setTimeout(() => {
+            if (rootEl) {
+              rootEl.innerHTML = ``;
+              rootEl.innerHTML += `<h2>state: ${args[0].state}</h2>`;
+              resolve("ok");
+            }
+          }, sec);
+        }
+      });
+    };
